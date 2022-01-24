a2e9d9ba467d93fd5c75e53c880c90474c49b01b
vetrivel12345 <arunvetri001@gmail.com>
2022-01-24 17:08:16 +0530

    commit check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { DatePicker } from 'antd';
 import Home from './Home';
 
 
 function App() {
   return (
     <div className="App">
-    <Home></Home>
+    <Home/>
     </div>
   );
 }
--- src/Nav_bar1.js
+++ src/Nav_bar1.js
@@ -17,6 +17,8 @@ function Nav_bar1() {
     const[items, setItems]=useState([])
     const[navBar,setNavBar]=useState([])
     const[value,setValue]=useState("")
+    const[catagory,setCatagory]=useState("")
+
     let collection = async()=>{
         let data =await  axios.get("https://rcz-backend-arvinth.herokuapp.com/api/shopData")
         setItems(data.data);
@@ -33,31 +35,38 @@ function Nav_bar1() {
         
     },[]);
     const summa=async()=>{
-        console.log("hi")
-        console.log(value)
-    }
+        items.map(e=>{
+            if(e.classification==value){
+                console.log(e.category)
+                console.log(...e.availableItems)
+            }
+
+        })
+    } 
+    useEffect(()=>{
+        summa()
+    },[value])
     let icon={
-        "Men": {dress},
-        "Women":{women},
-        "Kids": {shop},
-        "Home_Living":{gift}
+        "Home Living": "https://www.homeliving.my/image/homeliving/image/cache/data/WacwkD3K1623493542-499x281.png",
+        "Men":"https://cdn2.vectorstock.com/i/1000x1000/90/31/mens-style-logo-vector-21609031.jpg",
+        "Kids": "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/happy-kids-logo-design-template-1bd230063d3e309ca5d02d13529fde4c_screen.jpg?ts=1601458689",
+        "Women":"https://freedesignfile.com/upload/2017/10/Fashion-women-sign-with-logo-vectors-set-16.jpg"
     }
     const content = (
-        <div>
-          <p>Content</p>
-          <p>Content</p>
-        </div>
+         <div>
+         <p>prodect</p>
+         <p>prodect</p>
+         </div>
       );
     return (
         <div>
         <div style={{backgroundColor:"antiquewhite"}}>
             <Row style={{margin:"3em"}}>
             {navBar.map(e=>(
-                
                 <Popover content={content} title="Title" placement="bottom">
                 <Col span={5}>
-                    <Avatar size={40} src={dress}   />
-                 <Text value={e} style={{marginLeft:"1em"}} onMouseEnter={(e=>(setValue(e.target.getAttribute("value")))),summa}>{e}</Text>
+                    <Avatar size={50} src={icon[e]}/>
+                 <Text value={e} style={{marginLeft:"1em"}} onMouseEnter={(e=>(setValue(e.target.getAttribute("value"))))}>{e}</Text>
                  </Col>
                  </Popover>
             ))}
