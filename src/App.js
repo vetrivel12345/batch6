d4254ef18f75b4a877ba380921b084e5cc17243e
vetrivel12345 <arunvetri001@gmail.com>
2022-01-24 14:57:10 +0530

    commit check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { DatePicker } from 'antd';
-
 import Home from './Home';
 
 
--- src/Nav_bar1.js
+++ src/Nav_bar1.js
@@ -1,6 +1,6 @@
 import { Col, Row,Avatar, Image,Typography } from 'antd'
 import { UserOutlined } from '@ant-design/icons';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import dress from './image/dress.jpg'
 import shoes from './image/shoes.jpg'
 import women from './image/women.jpg'
@@ -8,31 +8,60 @@ import shop from './image/shop.jpg'
 import gift from './image/gift.jpg'
 import gem from './image/gem.jpg'
 import bg from './image/bg.jpg'
+import axios from 'axios'
+import { Popover, Button } from 'antd';
+
 
 function Nav_bar1() {
     const { Text, Link } = Typography;
+    const[items, setItems]=useState([])
+    const[navBar,setNavBar]=useState([])
+    const[value,setValue]=useState("")
+    let collection = async()=>{
+        let data =await  axios.get("https://rcz-backend-arvinth.herokuapp.com/api/shopData")
+        setItems(data.data);
+        let ItemsWithutDub=[];
+        let ItemsWithDub=data.data.map(e=>{
+            if(!ItemsWithutDub.includes(e.classification)){
+                ItemsWithutDub.push(e.classification)
+            }
+        })
+       setNavBar(ItemsWithutDub)
+    }
+    useEffect(()=>{
+        collection()
+        
+    },[]);
+    const summa=async()=>{
+        console.log("hi")
+        console.log(value)
+    }
+    let icon={
+        "Men": {dress},
+        "Women":{women},
+        "Kids": {shop},
+        "Home_Living":{gift}
+    }
+    const content = (
+        <div>
+          <p>Content</p>
+          <p>Content</p>
+        </div>
+      );
     return (
         <div>
         <div style={{backgroundColor:"antiquewhite"}}>
             <Row style={{margin:"3em"}}>
-            <Col span={4}>
-            <Avatar  size="large" src={<Image src={dress} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>Clothing </Text>
-            </Col>
-            <Col span={4}> 
-            <Avatar  size="large" src={<Image src={shoes} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>Shoes </Text>
-            </Col>
-            <Col span={4}>
-            <Avatar size="large"  src={<Image src={women} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>Active wears </Text>
-            </Col>
-            <Col span={4}>
-            <Avatar  size="large" src={<Image src={shop} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>accessories </Text>
-            </Col>
-            <Col span={4}>
-            <Avatar  size="large" src={<Image src={gift} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>Gift </Text>
-            </Col>
-            <Col span={4}>
-            <Avatar  size="large" src={<Image src={gem} style={{ width: 40 }} />}style={{margin:"1em"}} /> <Text>Inspiration </Text>
-            </Col>
+            {navBar.map(e=>(
+                
+                <Popover content={content} title="Title" placement="bottom">
+                <Col span={5}>
+                    <Avatar size={40} src={dress}   />
+                 <Text value={e} style={{marginLeft:"1em"}} onMouseEnter={(e=>(setValue(e.target.getAttribute("value")))),summa}>{e}</Text>
+                 </Col>
+                 </Popover>
+            ))}
+           
             </Row>
             </div>
             <div> <img src={bg} alt="" style={{width:"100%",borderRadius:"20px"}} /></div>
